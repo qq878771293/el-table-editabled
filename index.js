b3e69fa9b715ebb517db2887a82ef1ae222e67da
陈康 <672247608@qq.com>
2019-05-22 15:37:32 +0800

    update

--- index.js
+++
@@ -1,9 +0,0 @@
-import ElTableEditabled from './src/el-table-editabled.vue'
-import ElTableEditabledCell from './src/el-table-editabled-cell.vue'
-
-export default function (Vue) {
-  Vue.component(ElTableEditabled.name, ElTableEditabled)
-  Vue.component(ElTableEditabledCell.name, ElTableEditabledCell)
-
-  Vue.prototype.$$ElTableEditabled = new Vue()
-}
